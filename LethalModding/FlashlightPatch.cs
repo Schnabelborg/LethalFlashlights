1cd4a81190508b5e2e318dc67c02328c0259b32f
Schnabelborg <Schnabelborg30@gmail.com>
2024-01-11 21:59:47 +0100

    Added a config file and dependency on 'LethalConfig' to make explosion rate configurable

--- LethalModding/FlashlightPatch.cs
+++ LethalModding/FlashlightPatch.cs
@@ -1,6 +1,8 @@
 ﻿using HarmonyLib;
 using System;
 using BepInEx.Logging;
+using LethalModding;
+using BepInEx.Configuration;
 
 namespace LethalModding
 {
@@ -11,10 +13,12 @@ namespace LethalModding
         [HarmonyPatch(typeof(FlashlightItem), nameof(FlashlightItem.ItemActivate))] [HarmonyPrefix]
         public static void Prefix(FlashlightItem __instance)
         {
+            float explosionRate = LethalFlashlights.configExplosionRate.Value;
             Random rand = new Random();
-            int flashLightFailure = rand.Next(0, 9);
-            if (flashLightFailure == 0)
+            float flashLightFailure = (float)(rand.Next(0, 10000) / 100);
+            if (flashLightFailure <= explosionRate)
             {
+                logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID}: Flashlight breaky whoops");
                 UnityEngine.Vector3 playerpos = GameNetworkManager.Instance.localPlayerController.gameObject.transform.position;
                 Landmine.SpawnExplosion(playerpos, spawnExplosionEffect: true, 1f, 1f);
                 return;
--- LethalModding/Plugin.cs
+++ LethalModding/Plugin.cs
@@ -1,16 +1,31 @@
-﻿using System;
-using BepInEx;
+﻿using BepInEx;
 using HarmonyLib;
+using LethalConfig;
+using LethalConfig.ConfigItems.Options;
+using LethalConfig.ConfigItems;
+using BepInEx.Configuration;
 
 namespace LethalModding
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+    [BepInDependency("ainavt.lc.lethalconfig")]
+    public class LethalFlashlights : BaseUnityPlugin
     {
+        public static ConfigEntry<float> configExplosionRate;
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            configExplosionRate = Config.Bind("General", "ExplosionRate", 10f, "Changes the likelihood of your flashlight exploding");
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID}: Config generated!");
+            FloatStepSliderConfigItem explosionRateSlider = new FloatStepSliderConfigItem(configExplosionRate, new FloatStepSliderOptions
+            {
+                Min = 0f,
+                Max = 100f,
+                Step = 0.01f
+            });
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID}: Created config slider!");
+            LethalConfigManager.AddConfigItem(explosionRateSlider);
             Harmony.CreateAndPatchAll(typeof(FlashlightPatch), PluginInfo.PLUGIN_GUID);
         }
     }
